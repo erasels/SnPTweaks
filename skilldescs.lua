c990d50e0feca2638f40d579dc8f557fea3a16cb
erasels <emanuelbedburdick@yahoo.de>
2020-08-16 17:40:10 +0200

    Fixed wrong skill description assignment

--- skilldescs.lua
+++ skilldescs.lua
@@ -109,7 +109,7 @@ Hooks:Add("LocalizationManagerPostInit", "SnPTweaks_LocalizationManagerPostInit"
 		--Transporter
 		["menu_pack_mule_beta_desc"] = "BASIC: ##$basic##\nYou can throw bags ##50%## further.\n\nFor each ##10## armor points the bag movement penalty is reduced by ##1%##.\n\nACE: ##$pro##\nWhen your armor breaks, nearby enemies will be ignited for your maximum amount of armor.",
 		--Stockholm syndrom
-		["menu_pack_mule_beta_desc"] = "BASIC: ##$basic##\nCivilians are intimidated by the noise you make and remain intimidated ##50%## longer.\n\nYou gain ##10%## dodge chance near civilians.\n\nACE: ##$pro##\nYour hostages will not flee when they have been rescued by law enforcers. Whenever you get into custody, your hostages will trade themselves for your safe return. This effect can occur during assaults, but only ##1## time during a heist.\n\nYou gain an additional ##10%## dodge chance near civilians.",
+		["menu_stockholm_syndrome_beta_desc"] = "BASIC: ##$basic##\nCivilians are intimidated by the noise you make and remain intimidated ##50%## longer.\n\nYou gain ##10%## dodge chance near civilians.\n\nACE: ##$pro##\nYour hostages will not flee when they have been rescued by law enforcers. Whenever you get into custody, your hostages will trade themselves for your safe return. This effect can occur during assaults, but only ##1## time during a heist.\n\nYou gain an additional ##10%## dodge chance near civilians.",
 		
 		--PERKS
 		--Grinder
